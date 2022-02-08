cab48d4dea0e48a52928f2eb19130ae69510164c
Vahid Kiani <vdkkia2004@yahoo.com>
2022-02-08 13:26:00 +0100

    Add Firework's tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const Timer = (props) => {
+  const [timerValue, setTimerValue] = useState(props.initial);
+  const [timer, setTimer] = useState();
+
+  const handleReset = () => {
+    clearTimeout(timer);
+  };
+
+  useEffect(() => {
+    const t = setTimeout(() => {
+      if (timerValue > 0) setTimerValue(timerValue - 1);
+      if (timerValue === 0) {
+        clearTimeout(t);
+      }
+    }, 1000);
+    setTimer(t);
+  }, [timerValue]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-testid="app-title">
+      <div className="mt-100 layout-column align-items-center justify-content-center">
+        <div className="timer-value" data-testid="timer-value">
+          {timerValue}
+        </div>
+        <button className="large" data-testid="stop-button" onClick={handleReset}>
+          Stop Timer
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Timer;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,87 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { render, fireEvent, cleanup, act } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+var initialValue;
+
+afterEach(() => {
+  cleanup();
+});
+
+beforeEach(() => {
+  jest.clearAllTimers();
+  jest.useFakeTimers("modern");
+});
+
+test("initial UI is rendered as expected", () => {
+  initialValue = 60;
+  const renderApp = () => render(<App initial={initialValue} />);
+  let { getByTestId } = renderApp();
+  expect(getByTestId("app-title")).toHaveTextContent("Timer");
+  expect(getByTestId("timer-value")).toHaveTextContent("60");
+  expect(getByTestId("stop-button")).toHaveTextContent("Stop Timer");
+});
+
+test("inital value is set via props", async () => {
+  initialValue = 120;
+  const renderApp = () => render(<App initial={initialValue} />);
+  let { getByTestId } = renderApp();
+  expect(getByTestId("timer-value")).toHaveTextContent("120");
+
+  // This doesn't pass
+  // act(() => jest.advanceTimersByTime(10000));
+
+  // But this one passes:
+  for (let i = 0; i < 10; i++) {
+    act(() => jest.advanceTimersByTime(1000));
+  }
+  expect(getByTestId("timer-value")).toHaveTextContent("110");
+});
+
+test("timer stops at 0", () => {
+  initialValue = 5;
+  const renderApp = () => render(<App initial={initialValue} />);
+  let { getByTestId } = renderApp();
+  expect(getByTestId("timer-value")).toHaveTextContent("5");
+
+  // This doesn't pass
+  // act(() => jest.advanceTimersByTime(10000));
+
+  // But this one passes:
+  for (let i = 0; i < 10; i++) {
+    act(() => jest.advanceTimersByTime(1000));
+  }
+
+  expect(getByTestId("timer-value")).toHaveTextContent("0");
+});
+
+test("stop timer button stops the timer", () => {
+  initialValue = 50;
+  const renderApp = () => render(<App initial={initialValue} />);
+  let { getByTestId } = renderApp();
+  let stopTimerButton = getByTestId("stop-button");
+  expect(getByTestId("timer-value")).toHaveTextContent("50");
+
+  // This doesn't pass
+  // act(() => jest.advanceTimersByTime(10000));
+
+  // But this one passes:
+  for (let i = 0; i < 10; i++) {
+    act(() => jest.advanceTimersByTime(1000));
+  }
+
+  expect(getByTestId("timer-value")).toHaveTextContent("40");
+  fireEvent.click(stopTimerButton);
+  expect(getByTestId("timer-value")).toHaveTextContent("40");
+
+  // This doesn't pass
+  // act(() => jest.advanceTimersByTime(10000));
+
+  // But this one passes:
+  for (let i = 0; i < 10; i++) {
+    act(() => jest.advanceTimersByTime(1000));
+  }
+
+  expect(getByTestId("timer-value")).toHaveTextContent("40");
 });
